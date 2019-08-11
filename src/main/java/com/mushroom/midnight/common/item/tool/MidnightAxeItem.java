c3fb3d05031a2fcf50f1ec32e5be91743a7da5e8
gegy1000 <gegy1000@gmail.com>
2019-08-11 12:52:16 +0200

    Fix log block stripping

--- src/main/java/com/mushroom/midnight/common/item/tool/MidnightAxeItem.java
+++ src/main/java/com/mushroom/midnight/common/item/tool/MidnightAxeItem.java
@@ -1,57 +1,46 @@
 package com.mushroom.midnight.common.item.tool;
 
 import com.google.common.collect.ImmutableMap;
+import com.mushroom.midnight.Midnight;
 import com.mushroom.midnight.common.registry.MidnightBlocks;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.RotatedPillarBlock;
 import net.minecraft.item.AxeItem;
 import net.minecraft.item.IItemTier;
-import net.minecraft.item.ItemUseContext;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.SoundCategory;
-import net.minecraft.util.SoundEvents;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Map;
 
 public class MidnightAxeItem extends AxeItem {
-    private static final ImmutableMap<Block, Block> STRIPPABLE_BLOCKS;
-
     static {
-        ImmutableMap.Builder<Block, Block> builder = new ImmutableMap.Builder<>();
-        builder.put(MidnightBlocks.SHADOWROOT_LOG, MidnightBlocks.SHADOWROOT_STRIPPED_LOG);
-        builder.put(MidnightBlocks.DARK_WILLOW_LOG, MidnightBlocks.DARK_WILLOW_STRIPPED_LOG);
-        builder.put(MidnightBlocks.DEAD_WOOD_LOG, MidnightBlocks.DEAD_WOOD_STRIPPED_LOG);
-        STRIPPABLE_BLOCKS = builder.build();
+        try {
+            Field field = ObfuscationReflectionHelper.findField(AxeItem.class, "field_203176_a");
+
+            Field modifiersField = Field.class.getDeclaredField("modifiers");
+            modifiersField.setAccessible(true);
+            modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+
+            Map<Block, Block> strippableBlocks = (Map<Block, Block>) field.get(null);
+
+            ImmutableMap.Builder<Block, Block> builder = new ImmutableMap.Builder<>();
+            builder.putAll(strippableBlocks);
+
+            builder.put(MidnightBlocks.SHADOWROOT_LOG, MidnightBlocks.SHADOWROOT_STRIPPED_LOG);
+            builder.put(MidnightBlocks.DARK_WILLOW_LOG, MidnightBlocks.DARK_WILLOW_STRIPPED_LOG);
+            builder.put(MidnightBlocks.DEAD_WOOD_LOG, MidnightBlocks.DEAD_WOOD_STRIPPED_LOG);
+
+            builder.put(MidnightBlocks.SHADOWROOT_WOOD, MidnightBlocks.SHADOWROOT_STRIPPED_WOOD);
+            builder.put(MidnightBlocks.DARK_WILLOW_WOOD, MidnightBlocks.DARK_WILLOW_STRIPPED_WOOD);
+            builder.put(MidnightBlocks.DEAD_WOOD, MidnightBlocks.DEAD_WOOD_STRIPPED);
+
+            field.set(null, builder.build());
+        } catch (ReflectiveOperationException e) {
+            Midnight.LOGGER.warn("Failed to reflect strippable logs field", e);
+        }
     }
 
     public MidnightAxeItem(IItemTier tier, Properties properties) {
         super(tier, 6f, -3.2f, properties);
-    }
-
-    @Override
-    public ActionResultType onItemUse(ItemUseContext context) {
-        World world = context.getWorld();
-        BlockPos pos = context.getPos();
-        BlockState state = world.getBlockState(pos);
-        Block block = state.getBlock();
-
-        Block strippedBlock = STRIPPABLE_BLOCKS.getOrDefault(block, null);
-        if (strippedBlock == null) {
-            strippedBlock = BLOCK_STRIPPING_MAP.getOrDefault(block, null);
-        }
-        if (strippedBlock != null) {
-            world.playSound(context.getPlayer(), pos, SoundEvents.ITEM_AXE_STRIP, SoundCategory.BLOCKS, 1f, 1f);
-            if (!world.isRemote) {
-                world.setBlockState(pos, strippedBlock.getDefaultState().with(RotatedPillarBlock.AXIS, state.get(RotatedPillarBlock.AXIS)), 11);
-                if (context.getPlayer() != null) {
-                    context.getItem().damageItem(1, context.getPlayer(), player -> player.sendBreakAnimation(context.getHand()));
-                }
-            }
-
-            return ActionResultType.SUCCESS;
-        } else {
-            return ActionResultType.PASS;
-        }
     }
 }
