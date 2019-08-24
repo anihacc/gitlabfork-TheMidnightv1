89d7222e39bc829a70075f8919fec1d5f92e8eff
gegy1000 <gegy1000@gmail.com>
2019-08-24 19:29:22 +0200

    Fix game rule initializing to false in pre-existing world

--- src/main/java/com/mushroom/midnight/common/registry/MidnightGameRules.java
+++ src/main/java/com/mushroom/midnight/common/registry/MidnightGameRules.java
@@ -1,7 +1,28 @@
 package com.mushroom.midnight.common.registry;
 
+import com.google.common.base.Strings;
+import com.mojang.brigadier.arguments.BoolArgumentType;
 import net.minecraft.world.GameRules;
 
 public class MidnightGameRules {
-    public static final GameRules.RuleKey<GameRules.BooleanValue> DO_RIFT_SPAWNING = GameRules.register("doRiftSpawning", GameRules.BooleanValue.create(true));
+    public static final GameRules.RuleKey<GameRules.BooleanValue> DO_RIFT_SPAWNING = GameRules.register("doRiftSpawning", DefaultInitBooleanValue.create(true));
+
+    private static class DefaultInitBooleanValue extends GameRules.BooleanValue {
+        private DefaultInitBooleanValue(GameRules.RuleType<GameRules.BooleanValue> type, boolean value) {
+            super(type, value);
+        }
+
+        public static GameRules.RuleType<GameRules.BooleanValue> create(boolean value) {
+            return new GameRules.RuleType<>(BoolArgumentType::bool, type -> {
+                return new DefaultInitBooleanValue(type, value);
+            }, (s, v) -> { });
+        }
+
+        @Override
+        protected void func_223553_a(String value) {
+            if (!Strings.isNullOrEmpty(value)) {
+                super.func_223553_a(value);
+            }
+        }
+    }
 }
