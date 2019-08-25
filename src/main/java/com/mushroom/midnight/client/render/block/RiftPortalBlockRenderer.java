79e1bde244d05e4ecf3be6314a95bc22fbb36f21
gegy1000 <gegy1000@gmail.com>
2019-08-25 12:41:04 +0200

    Improve rift masks

--- src/main/java/com/mushroom/midnight/client/render/block/RiftPortalBlockRenderer.java
+++ src/main/java/com/mushroom/midnight/client/render/block/RiftPortalBlockRenderer.java
@@ -13,7 +13,10 @@ import org.lwjgl.opengl.GL11;
 
 public class RiftPortalBlockRenderer extends TileEntityRenderer<RiftPortalTileEntity> {
     private static final Minecraft CLIENT = Minecraft.getInstance();
-    private static final ResourceLocation MASK = new ResourceLocation(Midnight.MODID, "textures/effects/rift_portal_mask.png");
+    private static final ResourceLocation[] MASKS = new ResourceLocation[] {
+            new ResourceLocation(Midnight.MODID, "textures/effects/rift_portal_mask_1.png"),
+            new ResourceLocation(Midnight.MODID, "textures/effects/rift_portal_mask_2.png")
+    };
 
     private static final BlockEntityModel BLOCK_MODEL = new BlockEntityModel();
 
@@ -26,12 +29,14 @@ public class RiftPortalBlockRenderer extends TileEntityRenderer<RiftPortalTileEn
 
         BlockPos pos = entity.getPos();
         long seed = MathHelper.getCoordinateRandom(pos.getX(), pos.getY(), pos.getZ());
+        long textureSeed = seed ^ 8211203336981069197L;
+        long rotationSeed = seed ^ 526247544445692899L;
 
-        CLIENT.getTextureManager().bindTexture(MASK);
+        CLIENT.getTextureManager().bindTexture(MASKS[(int) (textureSeed & 1)]);
 
         GlStateManager.pushMatrix();
         GlStateManager.translated(x + 0.5, y + 0.5, z + 0.5);
-        GlStateManager.rotatef((seed & 3) * 90.0F, 0.0F, 1.0F, 0.0F);
+        GlStateManager.rotatef((rotationSeed & 3) * 90.0F, 0.0F, 1.0F, 0.0F);
 
         GlStateManager.disableBlend();
         GlStateManager.enableAlphaTest();
