cad52e6bd7eac176c467d0d6ab7a27946998ebbf
gegy1000 <gegy1000@gmail.com>
2019-08-29 13:38:05 +0200

    Simplify fungi inside logic

--- src/main/java/com/mushroom/midnight/common/block/FungiInsideBlock.java
+++ src/main/java/com/mushroom/midnight/common/block/FungiInsideBlock.java
@@ -2,12 +2,11 @@ package com.mushroom.midnight.common.block;
 
 import com.mushroom.midnight.common.registry.MidnightTags;
 import net.minecraft.block.AirBlock;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+import net.minecraft.world.IWorld;
 
 public class FungiInsideBlock extends AirBlock {
     public FungiInsideBlock(Properties properties) {
@@ -15,21 +14,19 @@ public class FungiInsideBlock extends AirBlock {
     }
 
     @Override
-    @SuppressWarnings("deprecation")
-    public void neighborChanged(BlockState state, World world, BlockPos pos, Block block, BlockPos fromPos, boolean isMoving) {
-        int neighborCount = this.countHatNeighbors(world, pos);
-        if (neighborCount <= 0) {
-            world.setBlockState(pos, Blocks.AIR.getDefaultState(), 3);
+    public BlockState updatePostPlacement(BlockState state, Direction facing, BlockState facingState, IWorld world, BlockPos currentPos, BlockPos facingPos) {
+        if (!this.hasHatNeighbor(world, currentPos)) {
+            return Blocks.AIR.getDefaultState();
         }
+        return state;
     }
 
-    private int countHatNeighbors(World world, BlockPos pos) {
-        int neighborCount = 0;
+    private boolean hasHatNeighbor(IWorld world, BlockPos pos) {
         for (Direction facing : Direction.values()) {
             if (world.getBlockState(pos.offset(facing)).isIn(MidnightTags.Blocks.FUNGI_HATS)) {
-                neighborCount++;
+                return true;
             }
         }
-        return neighborCount;
+        return false;
     }
 }
