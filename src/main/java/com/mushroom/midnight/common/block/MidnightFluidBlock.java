f9c5394688f6e9c775d691bea6085cd0106fa0e5
gegy1000 <gegy1000@gmail.com>
2019-09-18 18:28:18 +0200

    Fix not adding structures to structure map, miasma sounds & fix miasma reactivity

--- src/main/java/com/mushroom/midnight/common/block/MidnightFluidBlock.java
+++ src/main/java/com/mushroom/midnight/common/block/MidnightFluidBlock.java
@@ -1,21 +1,34 @@
 package com.mushroom.midnight.common.block;
 
+import com.mushroom.midnight.common.fluid.NeighborReactiveFluid;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.FlowingFluidBlock;
 import net.minecraft.fluid.FlowingFluid;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
+
+import java.util.function.Supplier;
 
 public class MidnightFluidBlock extends FlowingFluidBlock {
     private final boolean isBurning;
 
-    public MidnightFluidBlock(FlowingFluid fluid, boolean isBurning, Properties properties) {
+    public MidnightFluidBlock(Supplier<? extends FlowingFluid> fluid, boolean isBurning, Properties properties) {
         super(fluid, properties);
         this.isBurning = isBurning;
     }
 
     @Override
+    public boolean reactWithNeighbors(World world, BlockPos pos, BlockState state) {
+        FlowingFluid fluid = this.getFluid();
+        if (fluid instanceof NeighborReactiveFluid) {
+            return ((NeighborReactiveFluid) fluid).reactWithNeighbors(world, pos, state);
+        }
+        return true;
+    }
+
+    @Override
     public boolean isBurning(BlockState state, IBlockReader world, BlockPos pos) {
-        return isBurning;
+        return this.isBurning;
     }
 }
