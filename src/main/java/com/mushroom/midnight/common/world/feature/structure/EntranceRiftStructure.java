330873996fee09940aa0be762602fe8db06bb768
pentantan <shutux500@gmail.com>
2020-03-29 06:55:23 +0900

    rift only generate overworld or midnight

--- src/main/java/com/mushroom/midnight/common/world/feature/structure/EntranceRiftStructure.java
+++ src/main/java/com/mushroom/midnight/common/world/feature/structure/EntranceRiftStructure.java
@@ -1,6 +1,7 @@
 package com.mushroom.midnight.common.world.feature.structure;
 
 import com.mojang.datafixers.Dynamic;
+import com.mushroom.midnight.common.world.MidnightChunkGenerator;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.MutableBoundingBox;
@@ -8,6 +9,7 @@ import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.BiomeManager;
 import net.minecraft.world.gen.ChunkGenerator;
 import net.minecraft.world.gen.Heightmap;
+import net.minecraft.world.gen.OverworldChunkGenerator;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.NoFeatureConfig;
 import net.minecraft.world.gen.feature.structure.ScatteredStructure;
@@ -45,25 +47,27 @@ public class EntranceRiftStructure extends ScatteredStructure<NoFeatureConfig> {
 
     public boolean func_225558_a_(BiomeManager p_225558_1_, ChunkGenerator<?> p_225558_2_, Random p_225558_3_, int p_225558_4_, int p_225558_5_, Biome p_225558_6_) {
         ChunkPos chunkpos = this.getStartPositionForPosition(p_225558_2_, p_225558_3_, p_225558_4_, p_225558_5_, 0, 0);
-        if (p_225558_4_ == chunkpos.x && p_225558_5_ == chunkpos.z) {
-            int i = p_225558_4_ >> 4;
-            int j = p_225558_5_ >> 4;
-            p_225558_3_.setSeed((long) (i ^ j << 4) ^ p_225558_2_.getSeed());
-            p_225558_3_.nextInt();
-            if (p_225558_3_.nextInt(5) != 0) {
-                return false;
-            }
-
-            if (p_225558_2_.hasStructure(p_225558_6_, this)) {
-                for (int k = p_225558_4_ - 10; k <= p_225558_4_ + 10; ++k) {
-                    for (int l = p_225558_5_ - 10; l <= p_225558_5_ + 10; ++l) {
-                        if (Feature.VILLAGE.func_225558_a_(p_225558_1_, p_225558_2_, p_225558_3_, k, l, p_225558_1_.getBiome(new BlockPos((k << 4) + 9, 0, (l << 4) + 9)))) {
-                            return false;
-                        }
-                    }
+        if (p_225558_2_ instanceof OverworldChunkGenerator || p_225558_2_ instanceof MidnightChunkGenerator) {
+            if (p_225558_4_ == chunkpos.x && p_225558_5_ == chunkpos.z) {
+                int i = p_225558_4_ >> 4;
+                int j = p_225558_5_ >> 4;
+                p_225558_3_.setSeed((long) (i ^ j << 4) ^ p_225558_2_.getSeed());
+                p_225558_3_.nextInt();
+                if (p_225558_3_.nextInt(5) != 0) {
+                    return false;
                 }
 
-                return true;
+                if (p_225558_2_.hasStructure(p_225558_6_, this)) {
+                    for (int k = p_225558_4_ - 10; k <= p_225558_4_ + 10; ++k) {
+                        for (int l = p_225558_5_ - 10; l <= p_225558_5_ + 10; ++l) {
+                            if (Feature.VILLAGE.func_225558_a_(p_225558_1_, p_225558_2_, p_225558_3_, k, l, p_225558_1_.getBiome(new BlockPos((k << 4) + 9, 0, (l << 4) + 9)))) {
+                                return false;
+                            }
+                        }
+                    }
+
+                    return true;
+                }
             }
         }
 
