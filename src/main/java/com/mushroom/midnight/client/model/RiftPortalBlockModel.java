59b23829d86a3c4856d86704758a9a7263d85ab6
pentantan <shutux500@gmail.com>
2020-03-28 11:41:49 +0900

    add rift render

--- src/main/java/com/mushroom/midnight/client/model/RiftPortalBlockModel.java
+++ src/main/java/com/mushroom/midnight/client/model/RiftPortalBlockModel.java
@@ -18,7 +18,7 @@ public class RiftPortalBlockModel extends Model {
         this.textureWidth = 16;
         this.textureHeight = 16;
         this.shape = new ModelRenderer(this, 0, 0);
-        this.shape.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.shape.setRotationPoint(8.0F, 8.01F, 8.0F);
         this.shape.addBox(-8.0F, -8.0F, -8.0F, 16, 15, 16, 0.0F);
     }
 
