3d0f929803b5c3930f5228536d41b0499019bcf0
RedGalaxySoftware <onankman@gmail.com>
2020-04-09 17:05:07 +0200

    Comment out living render transformer as it was unused This only came up with a nonsense error message Fixes #375 until rifter dragging is reimplemented

--- src/main/resources/midnight_core.js
+++ src/main/resources/midnight_core.js
@@ -14,13 +14,13 @@ function initializeCoreMod() {
             },
             "transformer": patch_sound_source
         },
-        "LivingRendererTransformer": {
-            "target": {
-                "type": "CLASS",
-                "name": "net.minecraft.client.renderer.entity.LivingRenderer"
-            },
-            "transformer": patch_living_renderer
-        },
+//        "LivingRendererTransformer": {
+//            "target": {
+//                "type": "CLASS",
+//                "name": "net.minecraft.client.renderer.entity.LivingRenderer"
+//            },
+//            "transformer": patch_living_renderer
+//        },
         "BiomeColorsTransformer": {
             "target": {
                 "type": "CLASS",
@@ -57,26 +57,29 @@ function patch_sound_source(class_node) {
     return class_node;
 }
 
-function patch_living_renderer(class_node) {
-    var api = Java.type('net.minecraftforge.coremod.api.ASMAPI');
+// TODO: Re-implement this with rifter dragging behaviour
+// Commented out now to solve some nonsense error message
 
-    // TODO: What is this? func_77043_a doesn't show up anywhere in the MCP Mapping Viewer, not even as an unmapped function.
-    var apply_rotations_method = get_method(class_node, api.mapMethod("func_77043_a"));
-
-    var instructions = apply_rotations_method.instructions;
-    for (var i = 0; i < instructions.size(); i++) {
-        var insn = instructions.get(i);
-        if (insn instanceof MethodInsnNode && insn.owner.equals("com/mojang/blaze3d/platform/GlStateManager") && insn.name.equals("rotatef")) {
-            var insert = new InsnList();
-            insert.add(new VarInsnNode(Opcodes.ALOAD, 1));
-            insert.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "com/mushroom/midnight/client/ClientEventHandler", "onApplyRotations", "(Lnet/minecraft/entity/LivingEntity;)V", false));
-            instructions.insert(insn, insert);
-            break;
-        }
-    }
-
-    return class_node;
-}
+//function patch_living_renderer(class_node) {
+//    var api = Java.type('net.minecraftforge.coremod.api.ASMAPI');
+//
+//    // TODO: What is this? func_77043_a doesn't show up anywhere in the MCP Mapping Viewer, not even as an unmapped function.
+//    var apply_rotations_method = get_method(class_node, api.mapMethod("func_77043_a"));
+//
+//    var instructions = apply_rotations_method.instructions;
+//    for (var i = 0; i < instructions.size(); i++) {
+//        var insn = instructions.get(i);
+//        if (insn instanceof MethodInsnNode && insn.owner.equals("com/mojang/blaze3d/platform/GlStateManager") && insn.name.equals("rotatef")) {
+//            var insert = new InsnList();
+//            insert.add(new VarInsnNode(Opcodes.ALOAD, 1));
+//            insert.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "com/mushroom/midnight/client/ClientEventHandler", "onApplyRotations", "(Lnet/minecraft/entity/LivingEntity;)V", false));
+//            instructions.insert(insn, insert);
+//            break;
+//        }
+//    }
+//
+//    return class_node;
+//}
 
 function patch_biome_colors(class_node) {
     var api = Java.type('net.minecraftforge.coremod.api.ASMAPI');
