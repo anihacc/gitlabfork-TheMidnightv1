6055b9691652374c4b10d4595295d49c2d18974c
Jonathan Colmenares <jonathing.projects@gmail.com>
2020-04-09 17:46:07 -0400

    Ladder noise coremod transformer doesn't work. Disabled for now

---
+++ src/main/java/com/mushroom/midnight/common/LadderNoiseModifier.java
@@ -0,0 +1,17 @@
+//package com.mushroom.midnight.common;
+//
+//import net.minecraft.block.BlockState;
+//import net.minecraft.entity.Entity;
+//import net.minecraft.entity.LivingEntity;
+//import net.minecraft.util.math.BlockPos;
+//
+//public final class LadderNoiseModifier {
+//    public static boolean modifyLadderNoises(BlockState block, BlockPos pos, Entity e) {
+//        LivingEntity le = null;
+//        if (e instanceof LivingEntity) {
+//            le = (LivingEntity) e;
+//        }
+//
+//        return block.getBlock().isLadder(block, e.world, pos, le);
+//    }
+//}
