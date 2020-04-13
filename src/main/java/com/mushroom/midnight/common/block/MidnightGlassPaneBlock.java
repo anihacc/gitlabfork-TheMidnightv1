75ce5d44036597ee34593f7cfb9b403a5ab1dae5
RedGalaxySW <onankman@gmail.com>
2020-04-13 14:23:20 +0200

    Added beacon color for archaic glass

--- src/main/java/com/mushroom/midnight/common/block/MidnightGlassPaneBlock.java
+++ src/main/java/com/mushroom/midnight/common/block/MidnightGlassPaneBlock.java
@@ -1,18 +1,27 @@
 package com.mushroom.midnight.common.block;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.block.PaneBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorldReader;
+
+import javax.annotation.Nullable;
 
 public class MidnightGlassPaneBlock extends PaneBlock {
     public MidnightGlassPaneBlock() {
         super(Properties.create(Material.GLASS).sound(SoundType.GLASS).notSolid());
     }
 
-    /*@Override
-    @OnlyIn(Dist.CLIENT)
-    public BlockRenderLayer getRenderLayer()
-    {
-        return BlockRenderLayer.TRANSLUCENT;
-    }*/
+
+    private static final float[] BEACON_COLOR = {
+            177 / 255F, 143 / 255F, 217 / 255F
+    };
+
+    @Nullable
+    @Override
+    public float[] getBeaconColorMultiplier(BlockState state, IWorldReader world, BlockPos pos, BlockPos beaconPos) {
+        return BEACON_COLOR;
+    }
 }
