65cbfd1bde3d0e863790a890ddc0cbdb17b06479
RedGalaxySW <onankman@gmail.com>
2020-04-13 14:23:20 +0200

    Added beacon color for archaic glass

--- src/main/java/com/mushroom/midnight/common/block/MidnightGlassBlock.java
+++ src/main/java/com/mushroom/midnight/common/block/MidnightGlassBlock.java
@@ -7,6 +7,9 @@ import net.minecraft.block.material.Material;
 import net.minecraft.fluid.IFluidState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.ILightReader;
+import net.minecraft.world.IWorldReader;
+
+import javax.annotation.Nullable;
 
 public class MidnightGlassBlock extends GlassBlock {
     public MidnightGlassBlock() {
@@ -16,5 +19,16 @@ public class MidnightGlassBlock extends GlassBlock {
     @Override
     public boolean shouldDisplayFluidOverlay(BlockState state, ILightReader world, BlockPos pos, IFluidState fluidState) {
         return true;
+    }
+
+
+    private static final float[] BEACON_COLOR = {
+            177 / 255F, 143 / 255F, 217 / 255F
+    };
+
+    @Nullable
+    @Override
+    public float[] getBeaconColorMultiplier(BlockState state, IWorldReader world, BlockPos pos, BlockPos beaconPos) {
+        return BEACON_COLOR;
     }
 }
